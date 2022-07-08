a4320ec802b371fcfb2b2b765407e5175bef19dd
hedi <hedihamida98@gmail.com>
2022-07-09 00:02:34 +0100

    new

--- func_implem.c
+++ func_implem.c
@@ -1,40 +1,36 @@
 #include "main.h"
-char* itoa(int val, int base){
-    
-    static char buf[32] = {0};
-    
-    int i = 30;
-    
-    for(; val && i ; --i, val /= base)
-    
-        buf[i] = "0123456789abcdef"[val % base];
-    
-    return &buf[i+1];
-    
+char *itoa(int val, int base)
+{
+
+	static char buf[32] = {0};
+
+	int i = 30;
+
+	for (; val && i; --i, val /= base)
+
+		buf[i] = "0123456789abcdef"[val % base];
+
+	return &buf[i + 1];
 }
 int print_char(va_list *ap)
 {
-    char c;
+	char c;
 
-    c = va_arg(*ap, int);
-    write(1,&c,1);
-    return(1);
-    
+	c = va_arg(*ap, int);
+	write(1, &c, 1);
+	return (1);
 }
-int print_percent(va_list *ap) {
-    
-    char c;
+int print_percent(va_list *ap)
+{
 
-    c = va_arg(*ap, int);
-    write(1,"%",1);
-    return(1);
+		write(1, "%", 1);
+	return (1);
 }
-int print_newline(va_list *ap) {
-    char c;
+int print_newline(va_list *ap)
+{
 
-    c = va_arg(*ap, int);
-    write(1,"\n",1);
-    return(1);
+	write(1, "\n", 1);
+	return (1);
 }
 /**
  * print_int - print int
@@ -42,12 +38,12 @@ int print_newline(va_list *ap) {
  */
 int print_int(va_list *ap)
 {
-    int i;
+	int i;
 
-    i = va_arg(*ap, int);
-    
-    write(1,&i,4);
-    return(0);
+	i = va_arg(*ap, int);
+
+	write(1, &i, 4);
+	return (0);
 }
 /**
  * print_float - print float
@@ -55,12 +51,11 @@ int print_int(va_list *ap)
  */
 int print_float(va_list *ap)
 {
-    float f;
+	float f;
 
-    f = va_arg(*ap, double);
-    printf("%f", f);
-    return(0);
-    
+	f = va_arg(*ap, double);
+	printf("%f", f);
+	return (0);
 }
 /**
  * print_str - print str
@@ -68,16 +63,16 @@ int print_float(va_list *ap)
  */
 int print_str(va_list *ap)
 {
-    char *str;
+	char *str;
 
-    str = va_arg(*ap, char *);
+	str = va_arg(*ap, char *);
 
-    if (str == NULL)
-    {
-        str = "(nil)";
-    }
+	if (str == NULL)
+	{
+		str = "(nil)";
+	}
 
-    write(1,str,strlen(str));
-    
-    return(0);
+	write(1, str, strlen(str));
+
+	return (0);
 }
--- printf.c
+++ printf.c
@@ -3,34 +3,29 @@
 #include <string.h>
 int _printf(const char *format, ...)
 {
-    va_list ap;
-    int newLine=0;
-    
-    va_start(ap, format);
-    
-    
-    while (*format != '\0') {
-     
-        if (*format == '%') {
-            newLine = 1;
-          
-               
-          char k = *(format+1);
-            get_op_func(k)(&ap);
-               
-         
-            
-        }
-        
-        
-        format++;
-        
-    }
-    if (*(format - 1) == '\n' && newLine) {
-        write (1,"\n",1);
-    }
-    
-    va_end(ap);
-    return (0);
-    
+	va_list ap;
+	int newLine = 0;
+	char k;
+	va_start(ap, format);
+
+	while (*format != '\0')
+	{
+
+		if (*format == '%')
+		{
+			newLine = 1;
+
+			k = *(format + 1);
+			get_op_func(k)(&ap);
+		}
+
+		format++;
+	}
+	if (*(format - 1) == '\n' && newLine)
+	{
+		write(1, "\n", 1);
+	}
+
+	va_end(ap);
+	return (0);
 }
