f70495effc72cb5be813219b15a9865c6e216183
hedi <hedihamida98@gmail.com>
2022-07-09 00:06:12 +0100

    new

--- func_implem.c
+++ func_implem.c
@@ -22,13 +22,13 @@ int print_char(va_list *ap)
 }
 int print_percent(va_list *ap)
 {
-
-		write(1, "%", 1);
+	(void)ap;
+	write(1, "%", 1);
 	return (1);
 }
 int print_newline(va_list *ap)
 {
-
+	(void)ap;
 	write(1, "\n", 1);
 	return (1);
 }
