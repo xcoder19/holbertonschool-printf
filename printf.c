f37d4041a1eaca129421afb5945f1bd13e99695e
hedi <hedihamida98@gmail.com>
2022-07-09 13:30:35 +0100

    new

--- printf.c
+++ printf.c
@@ -3,54 +3,56 @@
 #include <string.h>
 int _printf(const char *format, ...)
 {
-	if (format != NULL)
+	va_list ap;
+	int len = 0;
+	char k;
+	int m = _strlen(format);
+	int flag = 0;
+	const char *p = format;
+
+	if (format == NULL)
+	{
+		return (-1);
+	}
+
+	va_start(ap, format);
+
+	while (*format != '\0')
 	{
 
-		va_list ap;
-		int len = 0;
-		char k;
-		int m = _strlen(format);
-		const char *p = format;
-
-		va_start(ap, format);
-
-		while (*format != '\0')
+		if (*format == '%')
 		{
+			flag = 1;
 
-			if (*format == '%')
+			k = *(format + 1);
+			if ((get_op_func(k)) != NULL)
 			{
 
-				k = *(format + 1);
-				if ((get_op_func(k)) != NULL)
-				{
-
-					len = len + (get_op_func(k)(&ap));
-				};
-			}
-			if (*format != '%' && *(format - 1) != '%' && *format != '\n')
-			{
-				write(1, format, 1);
-				len++;
-			}
-			if (*format == '\\')
-			{
-				write(1, format, 1);
-			}
-
-			format++;
+				len = len + (get_op_func(k)(&ap));
+			};
 		}
-		if (*(p + m - 1) == '\n')
+		if (*format != '%' && *(format - 1) != '%' && *format != '\n')
 		{
-			write(1, "\n", 1);
+			write(1, format, 1);
 			len++;
 		}
-
-		va_end(ap);
-		if (len == 0)
+		if (*format == '\n' && flag)
 		{
-			return (-1);
+			write(1, format, 1);
 		}
-		return (len);
+
+		format++;
 	}
-	return (-1);
+	if (*(p + m - 1) == '\n')
+	{
+		write(1, "\n", 1);
+		len++;
+	}
+
+	va_end(ap);
+	if (len == 0)
+	{
+		return (-1);
+	}
+	return (len);
 }
