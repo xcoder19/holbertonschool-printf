b500e48f844b8e18bdc9531d08f7e874f3427e5b
hedi <hedihamida98@gmail.com>
2022-07-11 12:09:28 +0100

    new

--- func_implem.c
+++ func_implem.c
@@ -73,14 +73,15 @@ int print_int(va_list *ap)
 
 char *convert(unsigned int num, int base)
 {
-	static char Representation[] = "0123456789ABCDEF";
-	static char buffer[50];
+	char Representation[] = "0123456789ABCDEF";
+	char buffer[50];
 	char *ptr;
 
 	ptr = &buffer[49];
 	*ptr = '\0';
 
 	do
+
 	{
 		*--ptr = Representation[num % base];
 		num /= base;
